ef81fcdc1a99c4e65df70d2a46b1d31a871b1f2a
Maro1810 <rayyanhussain611@gmail.com>
2025-12-09 21:25:07 -0800

    more tuning + added dilation and mask view

--- color_detection.py
+++ color_detection.py
@@ -6,8 +6,8 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # These are random values for now
-lower_bound = np.array([5, 60, 180])
-upper_bound = np.array([25, 200, 250])
+lower_bound = np.array([10, 60, 90])
+upper_bound = np.array([100, 230, 250])
 
 def BGR_TO_HSV(color):
     hsv = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
@@ -16,6 +16,8 @@ def BGR_TO_HSV(color):
 thing = BGR_TO_HSV(np.uint8([[[114, 158, 199]]]))
 
 print(thing)
+
+print(BGR_TO_HSV(np.uint8([[[97, 101, 14]]])))
 
 # Throw an error and exit if the video file cannot be opened (not applicable in this case)
 if not video_capture.isOpened():
@@ -42,6 +44,16 @@ while True:
     # as the color range
     mask = cv2.inRange(hsvFrame, lower_bound, upper_bound)
 
+    # These two lines of code will dilate the contours that are found in the picture, which can help
+    # eliminate noise
+    kernel = np.ones((4, 4), "uint8")
+
+    mask = cv2.dilate(mask, kernel)
+    
+    # Using cv2.bitwise_and() we can modify the frame so that it only displays the parts of the frame
+    # that match the desired color
+    blackout = cv2.bitwise_and(hsvFrame, hsvFrame, mask = mask)
+
     # This creates the contours with the mask we defined earlier. The parameter cv2.RETR_TREE is the contour 
     # retrieval mode, and the cv2.CHAIN_APPROX_NONE parameter is the contour approximation method, and we 
     # select none since we want to display all contours
@@ -55,6 +67,10 @@ while True:
 
     # Show the webcam feed
     cv2.imshow("Webcam Feed", frame)
+
+    # Shows the same feed but filters out anything that doesn't match the color
+    cv2.imshow("Mask", blackout)
+    
 
     # If the key pressed by the user is q, break out of the loop
     if cv2.waitKey(1) == ord('q'):
